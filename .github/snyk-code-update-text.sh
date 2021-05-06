a9115854e75420b97cc265d75ee2f90faff8b9a9
Bailey Everitt <72466036+bgeveritt-snyk@users.noreply.github.com>
2021-05-06 10:53:40 -0400

    Update snyk-code-update-text.sh

--- .github/snyk-code-update-text.sh
+++ .github/snyk-code-update-text.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 jq '.["runs"][0]["results"][]["locations"][].physicalLocation.artifactLocation.uri |= walk(if type == "string" then gsub("'"${WORKSPACE}"'"; "") else . end)' snyk-code.sarif > snyk-code.sarif.tmp && cp snyk-code.sarif.tmp snyk-code.sarif && rm snyk-code.sarif.tmp
-jq 'del(.["runs"][0]["results"][].message.text) | .["runs"][0]["results"][].message |= with_entries(if .key == "markdown" then .key = "text" else . end)' snyk-code.sarif > snyk-code.sarif.tmp && cp snyk-code.sarif.tmp snyk-code.sarif && rm snyk-code.sarif.tmp
+#jq 'del(.["runs"][0]["results"][].message.text) | .["runs"][0]["results"][].message |= with_entries(if .key == "markdown" then .key = "text" else . end)' snyk-code.sarif > snyk-code.sarif.tmp && cp snyk-code.sarif.tmp snyk-code.sarif && rm snyk-code.sarif.tmp
