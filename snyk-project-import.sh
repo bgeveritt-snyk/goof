90510db412bf4674d0bfa4f0c8e2187372166145
Bailey Everitt <72466036+bgeveritt-snyk@users.noreply.github.com>
2021-09-07 12:43:57 -0400

    Update snyk-project-import.sh

--- snyk-project-import.sh
+++ snyk-project-import.sh
@@ -1,18 +1,16 @@
-name: "Snyk Project Import"
-# Run workflow each time code is pushed to your repository. 
-on: push
-jobs:
-  build:
-    runs-on: ubuntu-latest
-    steps:
-      - uses: actions/checkout@v2   
-      - name: Snyk Branch Import
-        run:  sh ./snyk-project-import.sh
-        env:
-            WORKSPACE: ${{ github.workspace }}
-            OWNER: ${{ github.repository_owner }}
-            REPOSITORY: ${{ github.event.repository.name }}
-            BRANCH_NAME: ${GITHUB_REF##*/}
-            SNYK_TOKEN: ${{ secrets.SNYK_TOKEN }}
-            SNYK_ORG: ${{ secrets.SNYK_ORG }}
-            SNYK_INTEGRATION_ID: ${{ secrets.SNYK_INTEGRATION_ID }}
+#!/bin/bash
+# Snyk Project Import
+
+wget --no-check-certificate --quiet \
+  --method POST \
+  --timeout=0 \
+  --header 'Authorization: token $SNYK_TOKEN' \
+  --header 'Content-Type: application/json; charset=utf-8' \
+  --body-data '{
+  "target": {
+    "owner": "$OWNER",
+    "name": "$REPOSITORY",
+    "branch": "$BRANCH_NAME"
+  }
+}' \
+   'https://snyk.io/api/v1/org/$SNYK_ORG/integrations/$SNYK_INTEGRATION_ID/import'
