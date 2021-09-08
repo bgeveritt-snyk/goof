dad309749dce7b9536683852754ce4a4fb480795
Bailey Everitt <72466036+bgeveritt-snyk@users.noreply.github.com>
2021-09-08 14:59:18 -0400

    Create snyk-alert-merge.sh

---
+++ snyk-alert-merge.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Snyk Alert Merge
+
+projectId=$(curl -s POST 'https://snyk.io/api/v1/org/'$SNYK_ORG'/projects' \
+--header 'Authorization: token '$SNYK_TOKEN'' \
+--header 'Content-Type: application/json; charset=utf-8' \
+--data-raw '{ "filters": { "name": "'$REPOSITORY'("'$BASE_REF'", "type": "sast" } }' | jq -r '.projects[].id')
+
+curl -X POST -H 'Content-type: application/json' --data '{ "blocks": [ { "type": "section", "text": { "type": "mrkdwn", "text": "Branch `'$HEAD_REF'` merged in to `'$BASE_REF'`" } }, { "type": "section", "text": { "type": "mrkdwn", "text": "<https://app.snyk.io/project/'$projectId'|View Snyk Results>" } } ] }' $SLACK_WEBHOOK_URL
