0860ead97ac108cb3519d4d802d5ea66e6ac9ae2
Bailey Everitt <72466036+bgeveritt-snyk@users.noreply.github.com>
2021-09-08 13:33:36 -0400

    Update snyk-project-import.sh

--- snyk-project-import.sh
+++ snyk-project-import.sh
@@ -3,16 +3,13 @@
 
 BRANCH_NAME=${GITHUB_REF##*/}
 
-wget --no-check-certificate --quiet \
-  --method POST \
-  --timeout=0 \
-  --header 'Authorization: token '$SNYK_TOKEN'' \
-  --header 'Content-Type: application/json; charset=utf-8' \
-  --body-data '{
+curl -s POST 'https://snyk.io/api/v1/org/'$SNYK_ORG'/integrations/'$SNYK_INTEGRATION_ID'/import' \
+--header 'Authorization: token '$SNYK_TOKEN'' \
+--header 'Content-Type: application/json; charset=utf-8' \
+--data-raw '{
   "target": {
     "owner": "'$OWNER'",
     "name": "'$REPOSITORY'",
     "branch": "'$BRANCH_NAME'"
   }
-}' \
-   'https://snyk.io/api/v1/org/'$SNYK_ORG'/integrations/'$SNYK_INTEGRATION_ID'/import'
+}'
