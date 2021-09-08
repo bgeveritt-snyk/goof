4cef1c7db3d754b013b38b9a8e98ccc646cf1236
Bailey Everitt <72466036+bgeveritt-snyk@users.noreply.github.com>
2021-09-08 17:25:11 -0400

    Update snyk-alert-merge.sh

--- .github/workflows/scripts/snyk-alert-merge.sh
+++ .github/workflows/scripts/snyk-alert-merge.sh
@@ -4,6 +4,9 @@
 projectId=$(curl -s POST 'https://snyk.io/api/v1/org/'$SNYK_ORG'/projects' \
 --header 'Authorization: token '$SNYK_TOKEN'' \
 --header 'Content-Type: application/json; charset=utf-8' \
---data-raw '{ "filters": { "name": "'$REPOSITORY'("'$BASE_REF'", "type": "sast" } }' | jq -r '.projects[].id')
-echo $projectId
-curl -X POST -H 'Content-type: application/json' --data '{ "blocks": [ { "type": "section", "text": { "type": "mrkdwn", "text": "Branch `'$HEAD_REF'` merged in to `'$BASE_REF'`" } }, { "type": "section", "text": { "type": "mrkdwn", "text": "<https://app.snyk.io/project/'$projectId'|View Snyk Results>" } } ] }' $SLACK_WEBHOOK_URL
+--data-raw '{ "filters": { "name": "'$REPOSITORY'("'$BASE_REF')", "type": "sast" } }' | jq -r '.projects[].id')
+
+curl -s $SLACK_WEBHOOK_URL \
+--header 'Content-type: application/json' \
+--data-raw '{ "blocks": [ { "type": "section", "text": { "type": "mrkdwn", "text": "Branch `'$HEAD_REF'` merged in to `'$BASE_REF'`" } }, { "type": "section", "text": { "type": "mrkdwn", "text": "<https://app.snyk.io/project/'$projectId'|View Snyk Results>" } } ] }'
+#curl -X POST -H 'Content-type: application/json' --data '{ "blocks": [ { "type": "section", "text": { "type": "mrkdwn", "text": "Branch `'$HEAD_REF'` merged in to `'$BASE_REF'`" } }, { "type": "section", "text": { "type": "mrkdwn", "text": "<https://app.snyk.io/project/'$projectId'|View Snyk Results>" } } ] }' $SLACK_WEBHOOK_URL
