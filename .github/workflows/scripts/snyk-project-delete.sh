2de2107dffd77c6764b47c773a07cbd6cc4a6515
Bailey Everitt <72466036+bgeveritt-snyk@users.noreply.github.com>
2021-11-12 10:31:59 -0500

    Update snyk-project-delete.sh

--- .github/workflows/scripts/snyk-project-delete.sh
+++ .github/workflows/scripts/snyk-project-delete.sh
@@ -15,7 +15,7 @@ snykResponse=$(curl -s POST 'https://snyk.io/api/v1/org/'$SNYK_ORG'/projects' \
 }' | jq -r '.projects[].id')
 
 for project in $snykResponse; do
-  curl -s -X DELETE 'https://snyk.io/api/v1/org/'$SNYK_ORG'/'$project'' \
+  curl -s -X DELETE 'https://snyk.io/api/v1/org/'$SNYK_ORG'/project/'$project'' \
   --header 'Authorization: token '$SNYK_TOKEN'' \
   --header 'Content-Type: application/json'
 done
